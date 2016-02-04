0c89da1a631a110cba88109d5f35e13fcbc8ee7d
takecian <fujikidev@gmail.com>
2016-02-04 20:10:09 +0900

    Initial Commit

---
+++ SimpleInstantiate/SimpleInstantiate.h
@@ -0,0 +1,19 @@
+//
+//  SimpleInstantiate.h
+//  SimpleInstantiate
+//
+//  Created by FUJIKI TAKESHI on 2016/02/04.
+//  Copyright © 2016年 takecian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleInstantiate.
+FOUNDATION_EXPORT double SimpleInstantiateVersionNumber;
+
+//! Project version string for SimpleInstantiate.
+FOUNDATION_EXPORT const unsigned char SimpleInstantiateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleInstantiate/PublicHeader.h>
+
+
